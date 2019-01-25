7933a2d0bb37dab8275c898c9ac97eca565c7bda
vzla0094 <vzla0094@gmail.com>
2019-01-24 18:35:44 -0700

    Added coded so it can render each player move on the DOM

--- practica.js
+++ practica.js
@@ -1,3 +1,54 @@
-let gameBoard = {
-    gameboard: []
+const nodeMaker = (nodeType, parent, text) => {
+  const newNode = document.createElement(nodeType);
+  if (text) newNode.textContent = text;
+  parent.appendChild(newNode);
+  return newNode;
+}
+
+const game = (() => {
+  const gameBoard = Array.from(document.querySelectorAll(".box"));
+  const player1 = {name: "", weapon: "x",}
+  const player2 = {name: "", weapon: "o",}
+  let currentPlayer = Object.assign({}, player1);
+  const changeCurrPlayer = () => {
+    if (currentPlayer.weapon === player1.weapon){
+      currentPlayer = Object.assign({}, player2)
+    }else currentPlayer = Object.assign({}, player1);
+  }
+  const renderMove = (event) => {
+    if (event.target.textContent !== "") return; //this won't allow selecting a space that's already marked
+    nodeMaker('p', event.target, currentPlayer.weapon);
+    playGame(gameBoard);
+  }
+  gameBoard.forEach(e => e.addEventListener('click', renderMove));
+  return {changeCurrPlayer};
+}
+)();
+
+function playGame(gameBoard){
+  game.changeCurrPlayer();
+  const winArrMaker = (startIndex, increment) => {
+    const newArr = []
+    for(let i = startIndex; newArr.length < 3; i += increment){
+      newArr.push(gameBoard[i].textContent);
+    }
+    return newArr;
+  }
+  const winArrays = [
+    winArrMaker(0, 1),
+    winArrMaker(3, 1),
+    winArrMaker(6, 1),
+    winArrMaker(0, 3),
+    winArrMaker(1, 3),
+    winArrMaker(2, 3),
+    winArrMaker(0, 4),
+    winArrMaker(2, 2)
+  ]
+  for(let arr in winArrays){
+    if (winArrays[arr].every(e => e === 'x')){
+      let win;
+    }else if(winArrays[arr].every(e => e === 'y')){
+      let win;
+    }
+  }
 }
